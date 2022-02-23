f6b9249315275bb82d554ff4b48f88a9e014a03f
mathkin-dwarf <dwarf.is.engineer@gmail.com>
2022-02-24 05:57:15 +0900

    Refactoring variable.

--- basics/py/09_02_03_while_looplimit.py
+++ basics/py/09_02_03_while_looplimit.py
@@ -1,11 +1,11 @@
 # 無限ループ防止
 LOOP_LIMIT = 10
-MAX_LOOP = 0
+LOOP_COUNT = 0
 
 l = ['Alice', 'Bob', 'Charlie']
 i = 0
 # 変数iがリストlの数よりも小さい場合のみ繰り返す
-while (i < len(l)) & (MAX_LOOP < LOOP_LIMIT):
+while (i < len(l)) & (LOOP_COUNT < LOOP_LIMIT):
     if l[i] == 'Bob':
         print('!!BREAK!!')
         break
@@ -14,4 +14,4 @@ while (i < len(l)) & (MAX_LOOP < LOOP_LIMIT):
     
     # ここに「i += 1 」がなかった場合、無限ループになる
     
-    MAX_LOOP += 1
+    LOOP_COUNT += 1
